4e78632fb3bdb4d783bdd5d01b317de9bf88b485
Uri Mor <uri.mor@gmail.com>
2016-06-14 22:41:06 +0300

    Added basic js viewer. Reads a board.json file and display the board on the browser

---
+++ viewer/viewer.js
@@ -0,0 +1,107 @@
+$(document).ready(function documentReady() {
+    console.log("ready!!");
+    $.getJSON("board.json", function get_json(data){
+        var numberOfColumns = data["column-descriptors"].length;
+        var numberOfRows = data["column-descriptors"].length;
+        var lengthOfColumnDescriptors = maxSubArrayLength(data["column-descriptors"]);
+        var lengthOfRowDescriptors = maxSubArrayLength(data["row-descriptors"]);
+
+        padDescriptors();
+        createColumnDescriptors();
+        createRowDescriptors();
+        createMainBoard();
+
+        console.log(data);
+        console.log("number Of columns = " + numberOfColumns);
+        console.log("number Of rows = " + numberOfRows);
+        console.log("length of column descriptors = " + lengthOfColumnDescriptors);
+        console.log("length of row descriptors = " + lengthOfRowDescriptors);
+
+        function padDescriptors(){
+            padDescriptorsToMatchMaxLength(data["column-descriptors"]);
+            padDescriptorsToMatchMaxLength(data["row-descriptors"]);
+        }
+
+        function createColumnDescriptors(){
+            var $table = $("<table></table>");
+            for(var i = 0; i < lengthOfColumnDescriptors ; i++){
+                var $tableRow = $("<tr></tr>");
+                _(data["column-descriptors"]).forEach(function(descriptor){
+                    var $tableCell = $("<td></td>");
+                    $tableCell.text(descriptor[i]);
+                    $tableCell.addClass("cell descriptor-cell");
+                    $tableRow.append($tableCell);
+                })
+                $table.append($tableRow);
+                $table.addClass("line-descriptors-container");
+            }
+            $("#column-descriptors").append($table);
+        }
+
+        function createRowDescriptors(){
+            var $table = $("<table></table>");
+            _(data["row-descriptors"]).forEach(function(descriptor){
+                var $tableRow = $("<tr></tr>");
+                _(descriptor).forEach(function(value){
+                    var $tableCell = $("<td></td>");
+                    $tableCell.text(value);
+                    $tableCell.addClass("cell descriptor-cell");
+                    $tableRow.append($tableCell);
+                })
+                $table.append($tableRow);
+                $table.addClass("line-descriptors-container");
+            })
+            $("#row-descriptors").append($table);
+        }
+
+        function createMainBoard(){
+            var $table = $("<table></table>");
+            _(data["main-board"]).forEach(function(row){
+                var $tableRow = $("<tr></tr>");
+                _(row).forEach(function(square){
+                    var $tableCell = $("<td></td>");
+                    switch (square){
+                        case 0:
+                            $tableCell.text(" ");
+                            $tableCell.addClass("cell square-empty");
+                            break;
+                        case 1:
+                            $tableCell.text(" ");
+                            $tableCell.addClass("cell square-full");
+                            break;
+                        case 2:
+                            $tableCell.text("X");
+                            $tableCell.addClass("cell square-x");
+                            break;
+                    }
+                    $tableRow.append($tableCell);
+                })
+                $table.append($tableRow);
+                $table.addClass("main-board-container");
+            })
+            $("#main-board").append($table);
+        }
+
+        function padDescriptorsToMatchMaxLength (descriptorsArray) {
+            var lengthOfDescriptors = maxSubArrayLength(descriptorsArray);
+            _(descriptorsArray).forEach(function(descriptor){
+                var descriptorLength = descriptor.length;
+               for(var i = 0; i < lengthOfDescriptors - descriptorLength; i++){
+                    descriptor.unshift("");
+               }
+            });
+        }
+
+        function maxSubArrayLength(array) {
+            var maxColumnDescriptorLength = 0;
+            _(array).forEach(function (element) {
+                if (element.length > maxColumnDescriptorLength) {
+                    maxColumnDescriptorLength = element.length;
+                }
+            });
+            return maxColumnDescriptorLength;
+        }
+
+    });
+
+})
